be5fc794cdddcae49688bec225b11f4852738b60
CappeXII <75085646+CappeXII@users.noreply.github.com>
2023-05-18 10:53:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class GeM(torch.nn.Module):
         return torch.nn.functional.avg_pool2d(x.clamp(min=self.eps).pow(self.p), (x.size(-2), x.size(-1))).pow(1./self.p) 
 
 class LightningModel(pl.LightningModule):
-    def __init__(self, val_dataset, test_dataset, descriptors_dim=512, num_preds_to_save=0, save_only_wrong_preds=True, param=3):
+    def __init__(self, val_dataset, test_dataset, descriptors_dim=512, num_preds_to_save=0, save_only_wrong_preds=True, param=2.5):
         super().__init__()
         self.val_dataset = val_dataset
         self.test_dataset = test_dataset

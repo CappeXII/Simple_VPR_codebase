3af2dcf377e40bc58716b17b253c234464f82c3a
CappeXII <75085646+CappeXII@users.noreply.github.com>
2023-05-18 11:49:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ def get_datasets_and_dataloaders(args):
     test_dataset = TestDataset(dataset_folder=args.test_path)
     train_loader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=True)
     val_loader = DataLoader(dataset=val_dataset, batch_size=args.batch_size, num_workers=4, shuffle=False)
-    test_loader = DataLoader(dataset=test_dataset, batch_size=args.batch_size, num_workers=4, shuffle=False)https://github.com/CappeXII/Simple_VPR_codebase/blob/main/main.py
+    test_loader = DataLoader(dataset=test_dataset, batch_size=args.batch_size, num_workers=4, shuffle=False)
     return train_dataset, val_dataset, test_dataset, train_loader, val_loader, test_loader
 
 
